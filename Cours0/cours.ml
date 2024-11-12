89ce4f6780e36878bb9e7e1f1c472e0139b1f7f6
massinS <massinsadi2020@gmail.com>
2024-11-12 21:58:33 +0100

    last Course

--- Cours0/cours.ml
+++ Cours0/cours.ml
@@ -96,3 +96,33 @@ let is_it_Two (x : int ) : string =
       (*Cela va engendrer un erreur car 5 est un cas pas traité é *)
       print_string ( is_it_Three 5 );;
 
+
+      
+let y : int list=[1;2;3;4;5];;
+
+let is_list_empty ( y: int list ) : string =
+    match y with
+    | [] -> "le tableau est vide"
+    | h::t-> "le tableau n'est pas vide ";;
+
+     (* print_string (is_list_empty y);; *)
+
+
+    let rec taille_list ( aa : int list) : int =
+        match aa with
+       | [] -> 0
+       | h::t -> 1 + ( taille_list t ) ;;
+
+(*       print_int (taille_list y);; *)
+
+
+let xxx=258;;
+let rec sum (aa: int list) : int =
+     match aa with
+    | [] -> 0
+    | h::t -> h+ sum(t);;
+
+print_int (sum y );;
+
+
+
--- Main.ml
+++ Main.ml
@@ -1,29 +1,3 @@
 
 
-
-
-let y : int list=[1;2;3;4;5];;
-
-let is_list_empty ( y: int list ) : string =
-    match y with
-    | [] -> "le tableau est vide"
-    | h::t-> "le tableau n'est pas vide ";;
-
-     (* print_string (is_list_empty y);; *)
-
-
-    let rec taille_list ( aa : int list) : int =
-        match aa with
-       | [] -> 0
-       | h::t -> 1 + ( taille_list t ) ;;
-
-(*       print_int (taille_list y);; *)
-
-
-let xxx=258;;
-let rec sum (aa: int list) : int =
-     match aa with
-    | [] -> 0
-    | h::t -> h+ sum(t);;
-
-print_int (sum y );;
+let x = "Hello world ";;
